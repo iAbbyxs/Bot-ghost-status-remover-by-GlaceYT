14579095a5e1fde88b285e8660b563e3e4729b02
Abycita <166802389+iAbbyxs@users.noreply.github.com>
2024-08-31 02:05:11 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Jugando en realityzone.world 🏰","Jugando en RealityZone 🏰"];
 
 
 let currentIndex = 0;
